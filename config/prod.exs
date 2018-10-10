c91fd3af5577b8832bcd548457ee9198c047a2d3
John Cotton <john@1080people.com>
2018-10-10 15:21:18 -0400

    update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :chatter, ChatterWeb.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  check_origin: false,
+  check_origin: false
 
 # Do not print debug messages in production
 config :logger, level: :info

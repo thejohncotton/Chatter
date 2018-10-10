095eb7cee85dae2fb2f1b0a5751a9b296e3eb86f
John Cotton <john@1080people.com>
2018-10-10 15:18:23 -0400

    update prod config

--- lib/chatter_web/channels/user_socket.ex
+++ lib/chatter_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule ChatterWeb.UserSocket do
   channel "chat_room:lobby", ChatterWeb.ChatRoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000, check_origin: false
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
